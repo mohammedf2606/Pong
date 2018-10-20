5c3fa45c5fc3f3722c1c9c17834b5b9e8d585f44
Fahim Mohammed <mohammedf2606@gmail.com>
2018-10-20 14:30:02 +0100

    Update PongOnePlayer.py

--- PongOnePlayer.py
+++ PongOnePlayer.py
@@ -1,9 +1,9 @@
 from Pong import *
 
 def AI():
-    if ball_pos[1] < Pong.paddle2_pos:
+    if ball_pos[1] < Pong.paddle2_pos[1]:
         while ball_pos[1] != Pong.paddle2_pos[1]:
-            self.player2.center_y = self.player2.center_y - 3
-    if ball_pos[1] > HALF_PAD_HEIGHT:
-        while ball_pos[1] != HALF_PAD_HEIGHT:
-            self.player2.center_y = self.player2.center_y + 3
+            Pong.paddle2_pos[1] += 3
+    elif ball_pos[1] < Pong.paddle2_pos[1]:
+        while ball_pos[1] != Pong.paddle2_pos[1]:
+            Pong.paddle2_pos[1] += 3
