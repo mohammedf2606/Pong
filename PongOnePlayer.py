58eed77eed640084c6171f509433742f3d9426e1
Fahim Mohammed <mohammedf2606@gmail.com>
2018-11-07 13:09:33 +0000

    Delete PongOnePlayer.py

--- PongOnePlayer.py
+++
@@ -1,9 +0,0 @@
-from Pong import *
-
-def AI():
-    if ball_pos[1] < Pong.paddle2_pos[1]:
-        while ball_pos[1] != Pong.paddle2_pos[1]:
-            Pong.paddle2_pos[1] += 3
-    elif ball_pos[1] < Pong.paddle2_pos[1]:
-        while ball_pos[1] != Pong.paddle2_pos[1]:
-            Pong.paddle2_pos[1] += 3
